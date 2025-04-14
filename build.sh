51fa15e242faae4d49b286ba88a07ddb6d86c1c0
Julien Marusi <julien.marusi@bbee.fr>
2025-04-14 18:13:46 +0200

    fix cbuild

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker compose rm -f
-git submodule foreach git pull origin main
+# git submodule foreach git pull origin main
 # git submodule update --init --recursive
 docker compose build --no-cache
 docker compose up # -d
